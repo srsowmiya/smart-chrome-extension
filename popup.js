5a165cbb3fc382ccd474a37b894bb998536906d1
srsowmiya <sowmiyasundaram1040@gmail.com>
2026-01-27 20:56:22 +0530

    connection between popup.js and content.js is done

--- popup.js
+++ popup.js
@@ -1,4 +1,23 @@
-chrome.runtime.onMessage.addListener((m)=>{
-    const  msg=document.querySelector("#test")
-    msg.textContent=m.message
-})
+document.addEventListener("DOMContentLoaded", () => {
+
+  const output = document.querySelector("#test");
+
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+
+    chrome.tabs.sendMessage(
+      tabs[0].id,
+      { type: "GET_MESSAGE_INFO" },
+      (response) => {
+
+        if (response && response.title) {
+          output.textContent = response.title;
+        } else {
+          output.textContent = "Open a coding problem page";
+        }
+
+      }
+    );
+
+  });
+
+});
