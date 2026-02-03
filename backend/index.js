a36b0d3b758bb4db533f6c6e7818289e0250b5fb
srsowmiya <sowmiyasundaram1040@gmail.com>
2026-02-03 20:01:07 +0530

    api added

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,14 @@
 import express from "express";
 import cors from "cors";
+import dotenv from "dotenv"
+dotenv.config()
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+console.log(process.env?"API IS AVAILABLE " :"API IS MISSING")
 // test route
 app.get("/", (req, res) => {
   res.send("Backend is running successfully 🚀");
